3d27c72388062b2bd8c80e3f5faf973fad10f30f
Andrii Zadorozhnii <zador746@gmail.com>
2023-07-16 18:28:06 -0700

    m

--- js/script.js
+++ js/script.js
@@ -2,7 +2,10 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 // scroll sections
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
