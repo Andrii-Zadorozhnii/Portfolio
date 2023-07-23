2880417c54b1b6604531810c361b1a959aa97bb6
Andrii Zadorozhnii <zador746@gmail.com>
2023-07-22 22:19:14 -0700

    add

--- js/script.js
+++ js/script.js
@@ -25,6 +25,14 @@ window.onscroll = () => {
                 link.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active section for animation on scroll
+
+            sec.classList.add("show-animate");
+        }
+
+        // if want to use aniamtion that repeats on scroll use this
+        else{
+            sec.classList.remove("show-animate");
         }
     })
 
@@ -33,9 +41,9 @@ window.onscroll = () => {
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    // remove toggle icon and navbar when click navbar links (scroll)
+    // remove toggle icon and navbar when click navbar navLinks(scroll)
 
-
-    // animation footer on scroll
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 
 }
